fd80de3c33086ceea23fc3353155cb8a910389a7
useosc <admin@useosc.com>
2019-07-20 18:10:35 +0800

    first commit

---
+++ module.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * hello_banbanjia模块定义
+ *
+ * @author hellorobot
+ * @url
+ */
+defined('IN_IA') or exit('Access Denied');
+
+class Hello_banbanjiaModule extends WeModule {
+
+
+
+}
---
+++ site.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * hello_banbanjia模块微站定义
+ *
+ * @author hellorobot
+ * @url
+ */
+defined('IN_IA') or exit('Access Denied');
+
+class Hello_banbanjiaModuleSite extends WeModuleSite {
+
+
+
+
+}
