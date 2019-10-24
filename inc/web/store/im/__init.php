cf2ffed1dd3eea941692eef966f2cb512a2a4afe
useosc <admin@useosc.com>
2019-10-24 19:30:25 +0800

    web-im会话列表

---
+++ inc/web/store/im/__init.php
@@ -0,0 +1,7 @@
+<?php
+defined('IN_IA') or exit('Access Denied');
+global $_W;
+global $_GPC;
+if (0 < $_W["we7_hello_banbanjia"]["sid"]) {
+    $_W["we7_hello_banbanjia"]["store"] = store_fetch($_W["we7_hello_banbanjia"]["sid"]);
+}
